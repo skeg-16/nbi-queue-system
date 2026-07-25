f28be14263f4387873b1c58636e66e3e6358c174
Angel Lyn Camus <gelyn0731@gmail.com>
2026-07-26 00:30:45 +0800

    update directory

--- client/src/context/SocketContext.jsx
+++ client/src/context/SocketContext.jsx
@@ -1,7 +1,12 @@
 import React, { createContext, useContext, useEffect } from 'react';
 import { io } from 'socket.io-client';
 
-const socket = io({ autoConnect: false });
+const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
+
+const socket = io(BACKEND_URL, { 
+  autoConnect: false,
+  transports: ['websocket', 'polling'] 
+});
 
 const SocketContext = createContext(socket);
 
